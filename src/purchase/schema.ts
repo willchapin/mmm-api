20c6959148b0a8e28248e224c3854128f041bb14
Will Chapin <wrchapin@gmail.com>
2018-10-04 16:15:28 -0400

    2 spaces

--- src/purchase/schema.ts
+++ src/purchase/schema.ts
@@ -6,17 +6,17 @@ const userId = Joi.number().integer();
 const tagIds = Joi.array().items(Joi.number().integer());
 
 export const getPurchaseSchema = {
-    id: id.required()
+  id: id.required()
 };
 
 export const createPurchaseSchema = {
-    tagIds: tagIds.required(),
-    description,
+  tagIds: tagIds.required(),
+  description,
 };
 
 export const updatePurchaseSchema = {
-    id: id.required(),
-    description,
-    userId,
-    tagIds,
+  id: id.required(),
+  description,
+  userId,
+  tagIds,
 };
